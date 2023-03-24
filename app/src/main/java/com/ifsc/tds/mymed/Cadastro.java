e04ec6f88470330e7dfde53581a3c28c74878ee5
Bruno Calegaro <bcalegaro@gmail.com>
2023-03-24 15:24:48 -0300

    firebase auth e database

--- app/src/main/java/com/ifsc/tds/mymed/Cadastro.java
+++ app/src/main/java/com/ifsc/tds/mymed/Cadastro.java
@@ -2,63 +2,104 @@ package com.ifsc.tds.mymed;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cadastro#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.ifsc.tds.mymed.data.model.Usuario;
+
+import java.time.LocalDate;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Cadastro extends Fragment {
+    private FirebaseAuth mAuth; //acessa os recursos de autenticação do Firebase
+    private DatabaseReference mDatabase; //acessa os recursos do banco de dados do Firebase
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Cadastro() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PaginaInicial.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Cadastro newInstance(String param1, String param2) {
-        Cadastro fragment = new Cadastro();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private EditText nomeEditText;
+    private EditText emailEditText;
+    private EditText senhaEditText;
+    private EditText diaEditText;
+    private EditText mesEditText;
+    private EditText anoEditText;
+    private RadioButton termosRadioButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Initialize Firebase Auth and DataBaseRef to users
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference("usuarios");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro, container, false);
+        View view = inflater.inflate(R.layout.fragment_cadastro, container, false);
+        //Configura as referências aos componentes da UI
+        nomeEditText = view.findViewById(R.id.editTextNomeCompleto);
+        emailEditText = view.findViewById(R.id.editTextCadastroEmail);
+        senhaEditText = view.findViewById(R.id.editTextCadastroSenha);
+        diaEditText = view.findViewById(R.id.editTextDia);
+        mesEditText = view.findViewById(R.id.editTextMes);
+        anoEditText = view.findViewById(R.id.editTextAno);
+        termosRadioButton = view.findViewById(R.id.radioBtnTermos);
+        //Adiciona uma ação ao botão cadastrar
+        Button cadastrar = view.findViewById(R.id.buttonCdadastrar);
+        cadastrar.setOnClickListener(view1 -> fazerCadastro());
+        return view;
+    }
+
+    void fazerCadastro() {
+        //leitura dos dados
+        String nome = nomeEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String senha = senhaEditText.getText().toString();
+        int dia = Integer.parseInt(diaEditText.getText().toString());
+        int mes = Integer.parseInt(mesEditText.getText().toString());
+        int ano = Integer.parseInt(anoEditText.getText().toString());
+        LocalDate dataNascimento = LocalDate.of(ano, mes, dia);
+        Log.d("MyMed", "Data Nascimento:" + dataNascimento);
+        //cria um novo login firebase
+        mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.w("MyMed", "Falha ao criar novo Login: ", task.getException());
+            } else {
+                Log.d("MyMed", "Login criado com sucesso");
+                //Le o UUI do novo usuário criado
+                String uui = task.getResult().getUser().getUid();
+                //E cria um novo usuario
+                Usuario usuario = new Usuario(nome, email, dataNascimento.toString());
+                criarUsuario(uui, usuario);
+            }
+        });
+    }
+
+    void criarUsuario(String uui, Usuario usuario) {
+        mDatabase.child(uui).setValue(usuario).addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.w("MyMed", "Falha ao criar dados do usuario: ", task.getException());
+            } else {
+                Log.d("MyMed", "Usuario criado com sucesso");
+                goToLogin();
+            }
+        });
+    }
+
+    void goToLogin() {
+        NavController nav = Navigation.findNavController(getView());
+        nav.popBackStack();
     }
 }
