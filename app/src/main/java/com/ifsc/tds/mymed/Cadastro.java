78d3465c95fa4fc572f5833754f8799475823448
laisefh <laise.fh@aluno.ifsc.edu.br>
2023-05-02 17:30:39 -0300

    layout mudou

--- app/src/main/java/com/ifsc/tds/mymed/Cadastro.java
+++ app/src/main/java/com/ifsc/tds/mymed/Cadastro.java
@@ -3,10 +3,13 @@ package com.ifsc.tds.mymed;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,14 @@ public class Cadastro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro, container, false);
+        View view = inflater.inflate(R.layout.fragment_cadastro, container, false);
+        Button btnAdd = view.findViewById(R.id.btnAdicionar);
+        btnAdd.setOnClickListener(view1 -> terminarCadastro());
+        return view;
     }
+      void terminarCadastro(){
+        NavController nav = Navigation.findNavController(getView());
+          nav.navigate(R.id.action_paginaInicial_to_paginaInicial2);
+         }
+
 }
